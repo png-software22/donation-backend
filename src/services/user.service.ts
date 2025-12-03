6dcf9e9f6ad27e6d94f19bd15dd793b66c0af302
Gursimran Singh <gursimran.singh@trantorinc.com>
2025-12-03 21:18:42 +0530

    feat(add-host): VCB-1234 add known host UAT

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDTO } from 'src/dto/create-user.dto';
+import { City } from 'src/models/city.model';
 import { State } from 'src/models/state.model';
 import { User } from 'src/models/user.model';
 
@@ -9,9 +10,19 @@ export class UserService {
   constructor(
     @InjectModel(User)
     private readonly userModel: typeof User,
+    @InjectModel(City) private readonly cityModel: typeof City,
   ) {}
 
   async createUser(user: CreateUserDTO) {
+    const city = await this.cityModel.findOne({
+        where: { id: user.cityId, stateId: user.stateId },
+      });
+  
+      if (!city) {
+        throw new BadRequestException(
+          `City with id ${user.cityId} does not belong to state ${user.stateId}`,
+        );
+      }
     return this.userModel.create(user as any);
   }
   async getAllUsers() {
